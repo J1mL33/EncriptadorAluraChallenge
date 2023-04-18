168c9dc68452e6a092abf585a7a909c5542a45f6
J1mL33 <131090708+J1mL33@users.noreply.github.com>
2023-04-17 20:37:27 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -6,98 +6,88 @@ let resultado = "";
 const patron = /[^a-z ]/;
 
 function start() {
-    ingreso = document.getElementById("ingreso-texto");
-    conresultado = document.getElementById('con-resultado');
-    conresultado.style.display = 'none';
-    botonEncriptar = document.getElementById("encriptar");
-    botonEncriptar.addEventListener('click', Encriptar);
-    botonDesencriptar = document.getElementById("desencriptar");
-    botonDesencriptar.addEventListener('click', Desencriptar);
-    botonCopiar = document.getElementById("copiarTexto");
-    botonCopiar.addEventListener('click', CopiarTexto);
+  ingreso = document.getElementById("ingreso-texto");
+  conresultado = document.getElementById("con-resultado");
+  conresultado.style.display = "none";
+  botonEncriptar = document.getElementById("encriptar");
+  botonEncriptar.addEventListener("click", Encriptar);
+  botonDesencriptar = document.getElementById("desencriptar");
+  botonDesencriptar.addEventListener("click", Desencriptar);
+  botonCopiar = document.getElementById("copiarTexto");
+  botonCopiar.addEventListener("click", CopiarTexto);
 }
 
-
-function Encriptar () {
-    origen = ingreso.value;
-    let validar = origen.toString();
-    console.log(patron.test(validar));
-    if (!origen) {
-        alert("Por favor ingresa el texto")
+function Encriptar() {
+  origen = ingreso.value;
+  let validar = origen.toString();
+  console.log(patron.test(validar));
+  if (!origen) {
+    alert("Por favor ingresa el texto");
+  } else {
+    if (patron.test(validar)) {
+      alert("Ingresa solo minusculas sin acentos");
+      location.reload();
     } else {
-        if (patron.test(validar)) {
-            alert("Ingresa solo minusculas sin acentos");
-            location.reload();
-        } else {
-            for (let i = 0; i < origen.length; i++) {
-                for (let j = 0; j < letras.length; j++) {
-                    if (origen[i] == letras[j]) {
-                        destino[i] = codigos[j];
-                        break;
-                    } else {
-                        destino[i] = origen[i];
-                    }
-                }
-            }
-            resultado = destino.join('');
-            console.log(resultado);
-            if (resultado != "") {
-                enviarResultado(resultado);
-             }
+      for (let i = 0; i < origen.length; i++) {
+        for (let j = 0; j < letras.length; j++) {
+          if (origen[i] == letras[j]) {
+            destino[i] = codigos[j];
+            break;
+          } else {
+            destino[i] = origen[i];
+          }
         }
+      }
+      resultado = destino.join("");
+      console.log(resultado);
+      if (resultado != "") {
+        enviarResultado(resultado);
+      }
     }
+  }
 }
 
 function Desencriptar() {
-    origen = ingreso.value;
-    let validar = origen.toString();
-    console.log(patron.test(validar));
-    if (!origen) {
-        alert("Por favor ingresa el texto")
+  origen = ingreso.value;
+  let validar = origen.toString();
+  console.log(patron.test(validar));
+  if (!origen) {
+    alert("Por favor ingresa el texto");
+  } else {
+    if (patron.test(validar)) {
+      alert("Ingresa solo minusculas sin acentos");
+      location.reload();
     } else {
-        if (patron.test(validar)) {
-            alert("Ingresa solo minusculas sin acentos");
-            location.reload();
-        } else {
-            
-           // for (let i = 0; i < origen.length; i++) {
-                for (let j = 0; j < codigos.length; j++) {
-                    origen = origen.replaceAll(codigos[j], letras[j]);
-                   /* if (origen[i] == codigos[j]) {
-                        destino[i] = letras[j];
-                        break;
-                    } else {
-                        destino[i] = origen[i];
-                    }*/
-               // }
-            }
-            resultado = origen;
-            console.log(resultado);
-            if (resultado != "") {
-                enviarResultado(resultado);
-             }
-        }
+      for (let j = 0; j < codigos.length; j++) {
+        origen = origen.replaceAll(codigos[j], letras[j]);
+      }
+      resultado = origen;
+      console.log(resultado);
+      if (resultado != "") {
+        enviarResultado(resultado);
+      }
     }
+  }
 }
 
 function CopiarTexto() {
-    navigator.clipboard.writeText(resultado);
-    alert("Texto copiado al portapapeles");
-    LimpiaOrigen();
-  
-    console.log(resultado.value);
+  navigator.clipboard.writeText(resultado);
+  alert("Texto copiado al portapapeles");
+  LimpiaOrigen();
+
+  console.log(resultado.value);
 }
 
 function enviarResultado(texto) {
-    let muestraResultado = document.getElementById("resultado");
-    let noencuentratexto = document.getElementById("sin-resultado");
-    noencuentratexto.style.display = 'none';
-    conresultado.style.display = '';
-    muestraResultado.innerHTML = texto; 
+  let muestraResultado = document.getElementById("resultado");
+  let noencuentratexto = document.getElementById("sin-resultado");
+  noencuentratexto.style.display = "none";
+  conresultado.style.display = "";
+  muestraResultado.innerHTML = texto;
 }
 
 function LimpiaOrigen() {
-    ingreso.value = "";
+  ingreso.value = "";
 }
 
-window.addEventListener('load', start);
+window.addEventListener("load", start);
